a7f858a3dad31eaba6e118b7b83ea09bfe66844d
CloneWith <clonewith@qq.com>
2024-12-26 20:45:48 +0800

    Add result display after lottery stopped

--- app/page.tsx
+++ app/page.tsx
@@ -7,66 +7,73 @@ import Button from '@mui/material/Button'
 import SettingsIcon from '@mui/icons-material/Settings'
 import ChecklistIcon from '@mui/icons-material/Checklist';
 import Link from 'next/link'
+import ResultDisplay from "./components/ResultDisplay";
 
 interface Prize {
-  name: string
-  image: string
-  count: number
+    name: string
+    image: string
+    count: number
 }
 
 export default function Home() {
-  const [people, setPeople] = useState<string[]>([])
-  const [prizes, setPrizes] = useState<Prize[]>([])
-  const [winners, setWinners] = useState<{ name: string; prize: string }[]>([])
-  const [showWinners, setShowWinners] = useState(false)
+    const [people, setPeople] = useState<string[]>([])
+    const [prizes, setPrizes] = useState<Prize[]>([])
+    const [winners, setWinners] = useState<{ name: string; prize: string }[]>([])
+    const [showWinners, setShowWinners] = useState(false)
+    const [showResultAnimation, setShowResultAnimation] = useState(false)
 
-  useEffect(() => {
     const storedPeople = localStorage.getItem('people')
     const storedPrizes = localStorage.getItem('prizes')
     const storedWinners = localStorage.getItem('winners')
 
-    if (storedPeople) setPeople(JSON.parse(storedPeople))
-    if (storedPrizes) setPrizes(JSON.parse(storedPrizes))
-    if (storedWinners) setWinners(JSON.parse(storedWinners))
-  }, [])
+    useEffect(() => {
+        if (storedPeople) setPeople(JSON.parse(storedPeople))
+        if (storedPrizes) setPrizes(JSON.parse(storedPrizes))
+        if (storedWinners) setWinners(JSON.parse(storedWinners))
+    }, [])
 
-  useEffect(() => {
-    localStorage.setItem('prizes', JSON.stringify(prizes))
-    localStorage.setItem('winners', JSON.stringify(winners))
-  }, [prizes, winners])
+    useEffect(() => {
+        localStorage.setItem('prizes', JSON.stringify(prizes))
+        localStorage.setItem('winners', JSON.stringify(winners))
+    }, [prizes, winners])
 
-  const handleLotteryComplete = (newWinners: { name: string; prize: string }[]) => {
-    setWinners([...winners, ...newWinners])
-  }
+    const handleLotteryComplete = (newWinners: { name: string; prize: string }[]) => {
+        setWinners([...winners, ...newWinners])
+        setShowResultAnimation(true)
+    }
 
-  return (
-    <div className="min-h-screen">
-      {!showWinners ? (
-        <>
-          <LotteryDisplay
-            people={people} 
-            prizes={prizes} 
-            winners={winners} 
-            onLotteryComplete={handleLotteryComplete}
-            setPrizes={setPrizes}
-          />
-          <div className="fixed bottom-4 right-4 flex p-4 justify-center gap-4 flex-auto bg-white rounded-lg shadow-lg">
-            <Button variant="outlined" startIcon={<ChecklistIcon />} onClick={() => setShowWinners(true)}>查看中奖名单</Button>
-            <Link href="/admin">
-              <Button variant="contained" startIcon={<SettingsIcon />}>管理人员和奖品</Button>
-            </Link>
-          </div>
-        </>
-      ) : (
-        <WinnersList 
-          winners={winners} 
-          setWinners={setWinners}
-          setShowWinners={setShowWinners}
-          prizes={prizes}
-          setPrizes={setPrizes}
-        />
-      )}
-    </div>
-  )
+    return (
+        <div className="min-h-screen">
+            {!showWinners ? (
+                <div>
+                    <LotteryDisplay
+                        people={people}
+                        prizes={prizes}
+                        winners={winners}
+                        onLotteryComplete={handleLotteryComplete}
+                        setPrizes={setPrizes}
+                    />
+                    <ResultDisplay shown={showResultAnimation} setShowResult={setShowResultAnimation}
+                                   winners={winners}/>
+                    <div
+                        className="fixed bottom-4 right-4 flex p-4 justify-center gap-4 flex-auto bg-white rounded-lg shadow-lg">
+                        <Button variant="outlined" startIcon={<ChecklistIcon/>}
+                                onClick={() => setShowWinners(true)}>查看中奖名单</Button>
+                        <Link href="/admin">
+                            <Button variant="contained" startIcon={<SettingsIcon/>}>管理人员和奖品</Button>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <WinnersList
+                    winners={winners}
+                    setWinners={setWinners}
+                    setShowWinners={setShowWinners}
+                    prizes={prizes}
+                    setPrizes={setPrizes}
+                />
+            )}
+        </div>
+    )
 }
 
